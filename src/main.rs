46bee3d27ec129e8c93cb08ab681439ddca366df
ckab04 <ciceronkapenabm@gmail.com>
2024-01-06 22:32:58 +0100

    Write header section (attempt 1)

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,48 @@ struct DnsHeader{
 
 }
 
+impl DnsHeader{
+    fn pack(&self)-> Vec<u8>{
+        let mut buffer = vec![];
+        buffer.extend_from_slice(&self.id.to_be_bytes());
+        let values = (self.qr as u16) << 15
+        |(self.opcode as u16) << 11
+            | (self.aa as u16) << 10
+            | (self.tc as u16) << 9
+            | (self.rd as u16) << 8
+            | (self.ra as u16) << 7
+            | (self.z as u16) << 4
+            | (self.r_code as u16);
+        buffer.extend_from_slice(&values.to_be_bytes());
+        buffer.extend_from_slice(&self.qd_count.to_be_bytes());
+        buffer.extend_from_slice(&self.an_count.to_be_bytes());
+        buffer.extend_from_slice(&self.ns_count.to_be_bytes());
+        buffer.extend_from_slice(&self.ar_count.to_be_bytes());
+        buffer
+    }
+
+}
+
+impl Default for DnsHeader{
+    fn default() -> Self {
+        Self{
+            id: 1234,
+            qr: 1,
+            opcode: 0,
+            aa: 0,
+            tc: 0,
+            rd: 0,
+            ra: 0,
+            z: 0,
+            r_code: 0,
+            qd_count: 0,
+            an_count: 0,
+            ns_count: 0,
+            ar_count: 0,
+        }
+    }
+}
+
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
@@ -35,8 +77,8 @@ fn main() {
             Ok((size, source)) => {
                 println!("Received {} bytes from {}", size, source);
                 //let response = [];
-                let header_info = write_header_section();
-                let response = unsafe { any_as_u8_slice(&header_info) };
+                let header_info = DnsHeader::default();
+                let response = header_info.pack();
                 udp_socket
                     .send_to(&response, source)
                     .expect("Failed to send response");
@@ -47,32 +89,4 @@ fn main() {
             }
         }
     }
-}
-
-
-fn write_header_section()-> DnsHeader{
-
-    DnsHeader{
-        id: 1234,
-        qr: 1,
-        opcode: 0,
-        aa: 0,
-        tc: 0,
-        rd: 0,
-        ra: 0,
-        z: 0,
-        r_code: 0,
-        qd_count: 0,
-        an_count: 0,
-        ns_count: 0,
-        ar_count: 0,
-    }
-
-}
-
-unsafe fn any_as_u8_slice<T: Sized>(p: &T) -> &[u8] {
-    core::slice::from_raw_parts(
-        (p as *const T) as *const u8,
-        core::mem::size_of::<T>(),
-    )
 }
