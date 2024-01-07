cdfd79fed1630e9f79c53fe8bc474f1e4722eae7
ckab04 <ciceronkapenabm@gmail.com>
2024-01-07 17:25:38 +0100

    Write question section (attempt 1)

--- src/message.rs
+++ src/message.rs
@@ -89,8 +89,8 @@ impl DnsQuestion{
 
     fn conversion(&self) -> Vec<u8>{
         let mut encoded_value: Vec<u8> = Vec::new();
+        println!("{}", self.domain_name);
         let domain_name = self.domain_name.split_once('.').expect("Could not split the domain name");
-        println!("{:?}", domain_name);
         let label1_len = domain_name.0.len();
         let label2_len = domain_name.1.len();
         encoded_value.extend_from_slice(format!("{:x}", label1_len).as_bytes());
