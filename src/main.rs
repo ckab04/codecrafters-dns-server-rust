af4a474b9f7249d5c98a28da4cc3002a7294d1a3
ckab04 <ciceronkapenabm@gmail.com>
2024-01-10 12:13:17 +0100

    Parse header section(attempt 1)

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,8 @@ fn main() {
                 let question = q.to_bytes();
                 //let question = DnsQuestion::default().to_bytes();
                 let answer = DnsAnswer::get_answer();
-                let header_packet = String::from_utf8(buf.to_vec()).expect("Could not convert buffer to string");
+                let temp = &buf[0..size];
+                let header_packet = String::from_utf8(temp.to_vec()).expect("Could not convert buffer to string");
                 println!("DNS PACKET: {header_packet}", );
 
                 response.extend_from_slice(&question);
